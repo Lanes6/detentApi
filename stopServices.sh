e3e3cbe8fc9a41c1adecf21ad5f747e5d1ac708b
Lanes <aguenier.guillaume@gmail.com>
2019-11-24 01:24:57 +0100

    d

--- stopServices.sh
+++ stopServices.sh
@@ -3,7 +3,7 @@ echo "START STOP SERVICE" &&
 mapfile < /var/serverScript/config/config.txt configs &&
 passwordSudoConf="$(echo -e "${configs[4]}" | tr -d '[:space:]')" &&
 echo "STOP GEOSERVER" &&
-#sh /usr/local/apache-tomcat9/bin/shutdown.sh &&
+sh /usr/local/apache-tomcat9/bin/shutdown.sh &&
 echo "STOP API" &&
 echo passwordSudoConf | sudo -S sh /etc/init.d/apache2 stop &&
 echo "STOP ROUTINES"

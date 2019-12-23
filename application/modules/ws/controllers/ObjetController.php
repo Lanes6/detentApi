a15c046def2f1ea3906ef82ca5097e8f446ca618
Diallo Mamadou <mamadou.diallo6@etu.univ-orleans.fr>
2019-12-23 12:40:04 +0100

    Partie Objet OK !

--- application/modules/ws/controllers/ObjetController.php
+++ application/modules/ws/controllers/ObjetController.php
@@ -58,21 +58,66 @@ class ObjetController
     }
 
 
-    public function deleteAction(){
+    public function updateAction(){
 
-        if(isset($_POST["id_objet"])){
+        $id_objet=$_POST["id_objet"];
+        $newType=$_POST["newType"];
+        $newDescription=$_POST["newDescription"];
+
+        if(isset($id_objet) && isset($newType) && isset($newDescription)){
             if($this->ctlToken()){
-                $id_user=$jwt->giveMePayload()->id_user;
+                $id_user=$this->jwt->giveMePayload()->id_user;
             }else{
                 $this->_return["msg"]="Vous n'êtes pas connecté !";
                 http_response_code(400);
             }
+            $objet=$this->_objetMapper->findByIdObjet($id_objet);
+            if($objet != NULL){
+                if($objet->getId_User() == $id_user){
+                    $newObjet= new Objet($id_user,$newType, $newDescription, $objet->getGeom());
+                    $newObjet->setId_Objet($id_objet);
+
+                    $res=$this->_objetMapper->updateObjet($newObjet);
+
+                    if($res){
+                        $this->_return["msg"]="L'objet ".$newObjet->getType()." à été bien modifié !";
+                        http_response_code(200);
+                    }else{
+                        $this->_return["msg"]="Erreur lors de la modification de l'objet";
+                        http_response_code(404);
+                    }
+                }else{
+                    $this->_return["msg"]="Vous ne pouvez pas modifier un objet que vous n'avez pas créé";
+                    http_response_code(403);
+                }
+            }else{
+                $this->_return["msg"]="Oups: Objet non trouvé !";
+                http_response_code(404);
+            }
+        }else{
+            $this->_return["msg"]="1 ou plusieurs parametres absents";
+            http_response_code(400);
+        }
+        echo(json_encode($this->_return));
+    }
+
+
+    public function deleteAction(){
+
+        if(isset($_POST["id_objet"])){
+            if($this->ctlToken()){
+                $id_user=$this->jwt->giveMePayload()->id_user;
+            }else{
+                $this->_return["msg"]="Vous n'êtes pas connecté !";
+                http_response_code(400);
+            }
+
             $id_objet=$_POST["id_objet"];
-            $objet=$this->_objetMapper->findByIdgin($id_objet);
+            $objet=$this->_objetMapper->findByIdObjet($id_objet);
             if($objet != NULL){
                 $res=$this->_objetMapper->deleteObjet($id_objet);
                 if($res){
-                    $this->_return["msg"]=$objet->getType()." à été bien supprime";
+                    $this->_return["msg"]="L'objet de type ".$objet->getType()." a ete bien supprime";
                     http_response_code(200);
                 }else{
                     $this->_return["msg"]="Problème lors de la suppression de l'objet";
@@ -108,53 +153,6 @@ class ObjetController
             }
         }else{
             $this->_return["msg"]="Parametre id_objet absent";
-            http_response_code(400);
-        }
-        echo(json_encode($this->_return));
-    }
-
-
-    public function updateAction(){
-
-        $id_objet=$_POST["id_objet"];
-        $newType=$_POST["newType"];
-        $newDescription=$_POST["newDescription"];
-
-        if(isset($id_objet) && isset($newType) && isset($newDescription)){
-            if($this->ctlToken()){
-                $id_user=$this->jwt->giveMePayload()->id_user;
-            }else{
-                $this->_return["msg"]="Vous n'êtes pas connecté !";
-                http_response_code(400);
-            }
-
-            $objet=$this->_objetMapper->findByIdObjet($id_objet);
-
-            if($objet != NULL){
-                if($objet->getId_User() == $id_user){
-
-                    $newObjet= new Objet($id_user,$newType, $newDescription, $objet->getGeom());
-                    $newObjet->setId_Objet($objet->getId_Objet());
-
-                    $res=$this->_objetMapper->updateObjet($newObjet);
-
-                    if($res){
-                        $this->_return["msg"]="L'objet ".$newObjet->getType()." à été bien modifié !";
-                        http_response_code(200);
-                    }else{
-                        $this->_return["msg"]="Erreur lors de la modification de l'objet";
-                        http_response_code(404);
-                    }
-                }else{
-                    $this->_return["msg"]="Vous ne pouvez pas modifier un objet que vous n'avez pas créé";
-                    http_response_code(403);
-                }
-            }else{
-                $this->_return["msg"]="Oups: Objet non trouvé !";
-                http_response_code(404);
-            }
-        }else{
-            $this->_return["msg"]="1 ou plusieurs parametres absents";
             http_response_code(400);
         }
         echo(json_encode($this->_return));
